6a704dba64029afdf9d38480c7fe71c6449b1144
hundteezy <dhund.data@gmail.com>
2024-06-11 15:27:21 +0100

    added count of all chars, forcing lowercase before count

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    num_char = get_char_count(text)
     print(f"{num_words} words found in the document")
+    print(num_char)
 
 def get_num_words(text):
     words = text.split()
@@ -12,5 +14,19 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
     
+def get_char_count(text):
+    lowered_chars = {}
+
+    
+    for chars in text:
+        lowered = chars.lower()
+        if lowered in lowered_chars:
+            lowered_chars[lowered] += 1
+        else:
+            lowered_chars[lowered] = 1
+    return lowered_chars
+
+
+
 
 main()
