09f74972a42445b2d4a9e98bd021c978e4053e8e
hundteezy <dhund.data@gmail.com>
2024-06-12 18:59:17 +0100

    formatted output alphabetically, sorting/count per line

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_char = get_char_count(text)
+    book_report = send_book_report(num_char)
+    ## print(num_char)
+    print("--- Begin report of books/frankenstein.txt --- ")
     print(f"{num_words} words found in the document")
-    print(num_char)
+    print()
+
+    for item in book_report:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item ['num']} times")
+
+    print("--- End report ---")
 
 def get_num_words(text):
     words = text.split()
@@ -17,7 +27,6 @@ def get_book_text(path):
 def get_char_count(text):
     lowered_chars = {}
 
-    
     for chars in text:
         lowered = chars.lower()
         if lowered in lowered_chars:
@@ -25,6 +34,17 @@ def get_char_count(text):
         else:
             lowered_chars[lowered] = 1
     return lowered_chars
+
+def sort_char(num_char):
+        return num_char["num"]
+
+def send_book_report(num_chars_report):
+    sorted_letters = []
+
+    for char in num_chars_report:
+        sorted_letters.append({"char": char, "num": num_chars_report[char]})
+    sorted_letters.sort(reverse=True, key=sort_char)
+    return sorted_letters
 
 
 
