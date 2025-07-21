a8ef6d3df3fad2dd20dd213acc9c954c36b4632b
Danadis <dhund.data@gmail.com>
2025-07-21 17:49:19 +0100

    added extra books for final testing, sys import for streamlined args instead of hard coded paths

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import sys
+from stats import (
+        get_num_words,
+    )
+
+
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_char = get_char_count(text)
@@ -16,9 +26,8 @@ def main():
 
     print("--- End report ---")
 
-def get_num_words(text):
-    words = text.split()
-    return len(words)
+
+
 
 def get_book_text(path):
     with open(path) as f:
